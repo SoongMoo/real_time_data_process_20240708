416142f2119cb34980e3e2570ef6c8e077476ac1
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-07-12 11:36:22 +0900

    Add files via upload

---
+++ java/r_20240709/src/r_20240708/Ex18.java
@@ -0,0 +1,18 @@
+package r_20240708;
+
+public class Ex18 {
+	public static void main(String[] args) {
+		int score = 74;
+		if (score >= 90) {
+			System.out.println("A");
+		} else if (score >= 80) {
+			System.out.println("B");
+		} else if (score >= 70) {
+			System.out.println("C");
+		} else if (score >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
