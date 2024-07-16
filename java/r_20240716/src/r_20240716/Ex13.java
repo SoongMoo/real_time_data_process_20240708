20fa1436043fe9dbe2c4d62e0e41fe86181234c5
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-07-16 15:40:47 +0900

    Add files via upload

--- java/r_20240716/src/r_20240716/Ex13.java
+++ java/r_20240716/src/r_20240716/Ex13.java
@@ -7,24 +7,23 @@ public class Ex13 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("고객수를 입력하시오. ");
-		// 고객수
+		// 0. 고객수
 		int ownerNum = sc.nextInt();
 		Ex13_Account account [] = new Ex13_Account[ownerNum];
-		// 계좌 생성
+		// 1.계좌 생성
 		account[Ex13_Account.idx++] = new  Ex13_Account(100000, "이숭무", 100);
-		// 입금
+		// 3. 입금
 		System.out.print("입금 금액을 입력하시오. : ");
 		int money = sc.nextInt();
 		account[0].balance += money;
-		// 출금
+		// 4.출금
 		System.out.print("출금 금액을 입력하시오. : ");
 		money = sc.nextInt();
 		account[0].balance -= money;
-		// 잔액출력
+		// 2.계좌출력
 		for(Ex13_Account obj : account) {
+			if(obj == null) break;
 			obj.print();
-		}
-		
+		}	
 	}
-
 }
