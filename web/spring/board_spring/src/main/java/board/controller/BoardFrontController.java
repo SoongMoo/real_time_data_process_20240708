747bacf5a19ac4f6a5f765f5312a280cfd9a94c0
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-10-02 17:11:34 +0900

    Add files via upload

--- web/spring/board_spring/src/main/java/board/controller/BoardFrontController.java
+++ web/spring/board_spring/src/main/java/board/controller/BoardFrontController.java
@@ -1,11 +1,6 @@
 package board.controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
