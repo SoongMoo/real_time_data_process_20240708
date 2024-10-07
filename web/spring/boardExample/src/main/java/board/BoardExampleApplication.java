fbfc7170ced8f867dc9c2588b1cd7eecdabbe054
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-10-07 12:09:02 +0900

    Add files via upload

---
+++ web/spring/boardExample/src/main/java/board/BoardExampleApplication.java
@@ -0,0 +1,13 @@
+package board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardExampleApplication.class, args);
+	}
+
+}
