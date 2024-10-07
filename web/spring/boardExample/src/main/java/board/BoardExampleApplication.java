3d63d04e5f37179549eecae03b1c451849433df5
SoongMoo <46243469+SoongMoo@users.noreply.github.com>
2024-10-07 15:06:03 +0900

    Add files via upload

--- web/spring/boardExample/src/main/java/board/BoardExampleApplication.java
+++ web/spring/boardExample/src/main/java/board/BoardExampleApplication.java
@@ -2,12 +2,18 @@ package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 @SpringBootApplication
 public class BoardExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardExampleApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String index() {
+		return "thymeleaf/index";
 	}
 
 }
