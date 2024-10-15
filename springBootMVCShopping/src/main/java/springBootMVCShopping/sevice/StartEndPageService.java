0840fc210af28c6a2372d588d7d07116f7fbdabf
misolaptop1 <misolaptop1@172.16.110.46>
2024-10-15 14:49:32 +0900

    pagging 모듈화

---
+++ springBootMVCShopping/src/main/java/springBootMVCShopping/sevice/StartEndPageService.java
@@ -0,0 +1,36 @@
+package springBootMVCShopping.sevice;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import springBootMVCShopping.domain.StartEndPageDTO;
+
+@Service
+public class StartEndPageService {
+	public StartEndPageDTO execute(int page, int limit, String searchWord) {
+		int startRow = ((page - 1) * limit) + 1 ; // 11
+		int endRow = startRow + limit - 1; // 11 + 10 -1 
+		StartEndPageDTO sepDTO = new StartEndPageDTO(startRow, endRow, searchWord);
+		return sepDTO;
+	}
+	public void execute(int page, int limit, int count,String searchWord
+			, List list, Model model) {
+							//      2        12         25
+		int limitPage = 10 ; // 1 ~ 10 , 11 ~ 20 , 21 ~ 30,...                               
+		int startPageNum = (int)((double) page / limitPage + 0.95 - 1) * limitPage + 1;
+		int endPageNum = startPageNum + limitPage - 1;  //20
+		int maxPage = (int)((double)count / limit + 0.95); // 3
+		if(endPageNum > maxPage) endPageNum = maxPage;
+		/**/
+		if(searchWord == null) searchWord="";
+		model.addAttribute("list", list);
+		model.addAttribute("searchWord", searchWord);
+		model.addAttribute("page", page);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+		model.addAttribute("count", count);
+		model.addAttribute("maxPage", maxPage);
+	}
+}
--- springBootMVCShopping/src/main/java/springBootMVCShopping/sevice/member/MemberListService.java
+++ springBootMVCShopping/src/main/java/springBootMVCShopping/sevice/member/MemberListService.java
@@ -9,33 +9,21 @@ import org.springframework.ui.Model;
 import springBootMVCShopping.domain.MemberDTO;
 import springBootMVCShopping.domain.StartEndPageDTO;
 import springBootMVCShopping.mapper.MemberMapper;
+import springBootMVCShopping.sevice.StartEndPageService;
 
 @Service
 public class MemberListService {
 	@Autowired
 	MemberMapper memberMapper;
+	@Autowired
+	StartEndPageService startEndPageService;
 	public void execute(String searchWord,Integer page, Model model) {
 		// 한페이지에 몇개를 보여줄것인지?     ///     1 , 2
 		Integer limit = 3; 
-		int startRow = ((page - 1) * limit) + 1 ; // 11
-		int endRow = startRow + limit - 1; // 11 + 10 -1 
-		StartEndPageDTO sepDTO = new StartEndPageDTO(startRow, endRow, searchWord);
+		StartEndPageDTO sepDTO = startEndPageService.execute(page, limit, searchWord);
 		List<MemberDTO> list = 
 				memberMapper.memberSelectList(sepDTO);
 		Integer count = memberMapper.memberCount();
-		//                        2        12         25
-		int limitPage = 10 ; // 1 ~ 10 , 11 ~ 20 , 21 ~ 30,...
-		//                                 
-		int startPageNum = (int)((double) page / limitPage + 0.95 - 1) * limitPage + 1;
-		int endPageNum = startPageNum + limitPage - 1;  //20
-		int maxPage = (int)((double)count / limit + 0.95); // 2
-		if(endPageNum > maxPage) endPageNum = maxPage;
-		model.addAttribute("list", list);
-		model.addAttribute("searchWord", searchWord);
-		model.addAttribute("page", page);
-		model.addAttribute("startPageNum", startPageNum);
-		model.addAttribute("endPageNum", endPageNum);
-		model.addAttribute("count", count);
-		model.addAttribute("maxPage", maxPage);
+		startEndPageService.execute(page, limit, count, searchWord, list, model);
 	}
 }
