5986598c858e9380ada047e7ba4aab4c8b4f9f9f
misolaptop1 <misolaptop1@172.16.110.46>
2024-10-28 16:47:40 +0900

    파일 수정

--- springBootMVCShopping/src/main/java/springBootMVCShopping/sevice/goods/GoodsUpdateService.java
+++ springBootMVCShopping/src/main/java/springBootMVCShopping/sevice/goods/GoodsUpdateService.java
@@ -2,6 +2,8 @@ package springBootMVCShopping.sevice.goods;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -44,7 +46,7 @@ public class GoodsUpdateService {
 		URL resource = getClass().getClassLoader().getResource("static/upload");
 		String fileDir = resource.getFile();
 		System.out.println(fileDir);
-		if(!goodsCommand.getGoodsMainImage().getOriginalFilename().isEmpty()) {
+		if(goodsCommand.getGoodsMainImage() != null) {
 			// 2. 파일객체를 불러오기
 			MultipartFile mf = goodsCommand.getGoodsMainImage();
 			// 3. 파일이름 가져오기
@@ -55,6 +57,7 @@ public class GoodsUpdateService {
 			String storeName = UUID.randomUUID().toString().replace("-", "");
 			// 6. 새로운 파일명과 확장자 붙이기
 			String storeFileName =  storeName + extension;
+			System.out.println("storeFileName : " + storeFileName);
 			// 7. 파일객체 만들기
 			File file = new File(fileDir + "/" + storeFileName);
 			// 8. 파일 저장
@@ -67,9 +70,9 @@ public class GoodsUpdateService {
 			dto.setGoodsMainImage(originalFile);
 			dto.setGoodsMainStoreImage(storeFileName);
 		}
-		if(goodsCommand.getGoodsDetailImage().length > 0) {
-			String originalTotal = "";
-			String storeTotal = "";
+		String originalTotal = "";
+		String storeTotal = "";
+		if(!goodsCommand.getGoodsDetailImage()[0].getOriginalFilename().isEmpty()) {
 			//1. 디렉터리 정보
 			// 2. 파일객체를 불러오기
 			for(MultipartFile mf :  goodsCommand.getGoodsDetailImage()) {
@@ -100,10 +103,39 @@ public class GoodsUpdateService {
 		// 파일의 정보를 디비로 부터 가져오기
 		GoodsDTO goodsDTO = goodsMapper.selectOne(goodsCommand.getGoodsNum());
 		// session에 있는 정보를 디비로부터 제거
+		// 배열을 리스트로 받아옴
+		List<String> dbOrg =  new ArrayList<>(
+					Arrays.asList(goodsDTO.getGoodsDetailImage().split("[/`]"))
+				); 
+		List<String> dbStore = new ArrayList<>(
+					Arrays.asList(goodsDTO.getGoodsDetailStoreImage().split("[/`]"))
+				);
+		/// session에 있는 데이터를  디비와 비교헤ㅐ서 디비에 있는데이터 삭제
+		if(list != null) {
+			for(FileDTO fdto : list) {
+				for( String img  : dbOrg) {
+					if(fdto.getOrgFile().equals(img)) {
+						dbOrg.remove(fdto.getOrgFile());
+						dbStore.remove(fdto.getStoreFile());
+						break;
+					}
+				}
+			}
+		}
+		for(String img : dbOrg) originalTotal += img + "/";
+		for(String img : dbStore) storeTotal += img + "/";
+		dto.setGoodsDetailStoreImage(storeTotal);
+		dto.setGoodsDetailImage(originalTotal);
 		
-		
-		
-		//goodsMapper.goodsUpdate(dto);
+		int i = goodsMapper.goodsUpdate(dto);
+		if(i > 0) {
+			if(list != null) {
+				for(FileDTO fd : list) {
+					File file = new File(fileDir + "/" + fd.getStoreFile());
+					if(file.exists()) file.delete();
+				}
+			}
+		}
 	}
 }
 
